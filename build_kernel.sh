4d61c388ac3d8cdec7e2943e67d7bd353dbd2e79
Mohamedfullhd <aaamohamed312@gmail.com>
2025-07-11 21:58:48 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,8 +6,8 @@ sudo apt-get install gcc-aarch64-linux-gnu bc -y
 set -e
 
 KERNEL_DIR="$(pwd)"
-CHAT_ID="7861399342"
-TOKEN="8058298412:AAGZOl0D_6aYSLXSpfsdUAe-eJ5yoEQyCC0"
+CHAT_ID="7898438749"
+TOKEN="8188281304:AAGd1EB1FqT4NQjOgS7p4IfPyjYRXjHvIMw"
 DEVICE="gale"
 KERVER=$(make -s kernelversion | tr -d '[:space:]')
 VERSION=v1
